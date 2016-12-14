e719d978d9bd9c05e5a2dd06ec850925c8ec02be
Paul Scott <pscott@mobacar.com>
2016-12-14 08:38:05 +0000

    base bootstrapped codey

---
+++ src/main/java/slackbot/SlackbotApplication.java
@@ -0,0 +1,12 @@
+package slackbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlackbotApplication.class, args);
+	}
+}
